ab4be16578d699efd074e5da99564f78416ee4e2
ant <anth1y@gmail.com>
2015-06-12 04:01:07 +0000

    calling the /home/user and printing it out

--- mkgoenv.go
+++ mkgoenv.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
-	"path/filepath"
+	//	"syscall"
+	"fmt"
 )
 
-func visit(path string, f os.FileInfo, err error) error {
-	fmt.Printf("Visited: %s\n", path)
-	return nil
-}
-
 func main() {
-	flag.Parse()
-	root := flag.Arg(0)
-	err := filepath.Walk(root, visit)
-	fmt.Println("filepath.Walk() returned %v\n", err)
+	home := os.Getenv("HOME")
+	fmt.Println(home)
 }
